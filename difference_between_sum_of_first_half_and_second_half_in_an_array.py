99888724a001c18c0271b0146883f0dcad50335b
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-05-31 10:39:02 +0530

    a=int(input()) b=list(map(int,input().split())) c=0 v=0 if a%2==0:     for i in range(a//2):         c=c+b[i]     for i in range(a//2,a):         v=v+b[i] else:     b.append(0)     for i in range(a//2):         c=c+b[i]     for i in range(a//2,a):         v=v+b[i] print(abs(v-c))

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,17 @@
+a=int(input())
+b=list(map(int,input().split()))
+c=0
+v=0
+if a%2==0:
+    for i in range(a//2):
+        c=c+b[i]
+    for i in range(a//2,a):
+        v=v+b[i]
+else:
+    b.append(0)
+    for i in range(a//2):
+        c=c+b[i]
+    for i in range(a//2,a):
+        v=v+b[i]
+print(abs(v-c))
+        
