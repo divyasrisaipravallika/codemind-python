b6cd83770d4351ca57305318b0b88297a484b798
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-05-31 11:38:17 +0530

    def cou(n):     c=0     for i in range(2,n):         if n%i==0:             c=c+1     if n==1:return 0     elif n==2:return 1     elif c>0: return 0     else:return 1 a=int(input()) #print(a) b=list(map(int,input().split())) #print(b) c=int(input()) d=0 for i in range(a):     if b[i]>=c:         if cou(b[i])==1:             d=d+1 print(d)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,21 @@
+def cou(n):
+    c=0
+    for i in range(2,n):
+        if n%i==0:
+            c=c+1
+    if n==1:return 0
+    elif n==2:return 1
+    elif c>0: return 0
+    else:return 1
+a=int(input())
+#print(a)
+b=list(map(int,input().split()))
+#print(b)
+c=int(input())
+d=0
+for i in range(a):
+    if b[i]>=c:
+        if cou(b[i])==1:
+            d=d+1
+print(d)
+            
