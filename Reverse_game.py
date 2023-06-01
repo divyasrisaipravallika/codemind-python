2505af1fec5330ba5156ab2a3e04fb97be7b0879
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-06-01 13:33:15 +0530

    def pan(n):     c=0     rev=0     while n!=0:         c=n%10         rev=rev*10+c         n=n//10     return rev a=int(input()) b=list(map(int,input().split())) v=0 for i in range(a):     print(pan(b[i]),end=" ")

---
+++ Reverse_game.py
@@ -0,0 +1,14 @@
+def pan(n):
+    c=0
+    rev=0
+    while n!=0:
+        c=n%10
+        rev=rev*10+c
+        n=n//10
+    return rev
+a=int(input())
+b=list(map(int,input().split()))
+v=0
+for i in range(a):
+    print(pan(b[i]),end=" ")
+        
