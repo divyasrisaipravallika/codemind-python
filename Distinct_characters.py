c1cb850eab63edb92b31703e080368551721c4f9
divyasrisaipravallika <119841245+divyasrisaipravallika@users.noreply.github.com>
2023-06-02 18:56:44 +0530

    a=input() b=[] c=a.lower() for i in range(len(c)):     if c[i]!=" " and c.count(c[i])==1:         b.append(c[i]) #d=list(dict.fromkeys(b)) b.sort() for i in range(len(b)):     print(b[i],end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -2,9 +2,9 @@ a=input()
 b=[]
 c=a.lower()
 for i in range(len(c)):
-    if c[i]!=" ":
+    if c[i]!=" " and c.count(c[i])==1:
         b.append(c[i])
-d=list(dict.fromkeys(b))
-d.sort()
-for i in range(len(d)):
-    print(d[i],end="")
+#d=list(dict.fromkeys(b))
+b.sort()
+for i in range(len(b)):
+    print(b[i],end="")
